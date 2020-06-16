97ac73ed6f8c1e8d346be21e5b09208d16b397d4
augustus <dtminh188@gmail.com>
2020-06-16 09:24:58 +0700

    module user

--- app/ApiModules/Users/Controllers/UserController.php
+++ app/ApiModules/Users/Controllers/UserController.php
@@ -24,7 +24,23 @@ class UserController extends Controller
 
     public function index()
     {
-        dd(123);
-        return $this->userRepo->getPaginate();
+        return response()->json($this->userRepo->getPaginate());
+    }
+
+    public function store(Request $request)
+    {
+        $user = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => \Hash::make($request->password),
+        ];
+        return response()->json($this->userRepo->create($user));
+    }
+
+    public function show($id)
+    {
+        $user = $this->userRepo->firstWithPosts($id, ['id', 'name', 'email']);
+
+        return response()->json($user);
     }
 }
---
+++ app/ApiModules/Users/Models/Post.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\ApiModules\Users\Models;
+
+use App\Models\Post as Model;
+
+class Post extends Model
+{
+    //
+}
